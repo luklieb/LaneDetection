e818b03816c280a51a55c2716bc24e76dc24385e
Lukas L <Lukas@example.com>
2018-03-10 13:39:49 -0800

    changed parameterfile ending to .par defined fcts to create parameterfiles for each algorithm

--- eval/createParamFiles.py
+++ eval/createParamFiles.py
@@ -3,14 +3,9 @@ import os
 import shutil
 import itertools
 from pathlib import Path
+from directoryStructure import *
 
-resultsDirName = "./eval_results"
-imagesDirName = "./eval_images"
-gtDirName = "./eval_images/groundtruth"
-inputDirName = "./eval_images/input"
-tmpDirName = "./eval_images/tmp"
-paramDirName = "./eval_param"
-paramFileNameBase = "param_"
+
 resultsDir = Path(resultsDirName)
 gtDir = Path(gtDirName)
 inputDir = Path(inputDirName)
@@ -18,50 +13,20 @@ tmpDir = Path(tmpDirName)
 paramDir = Path(paramDirName)
 currentDir = Path.cwd
 
-
-
-if not (resultsDir.is_dir() and gtDir.is_dir() and inputDir.is_dir() and tmpDir.is_dir() and paramDir.is_dir()):
-    print("Error, your directory strcture is wrong! Aborting this script...")
-    print("You need the following structure:")
-    print("{} (empty) for the evaluation measurements files and parameter files".format(resultsDirName))
-    print("{} (empty) for the parameter files generated with this script".format(paramDirName))
-    print("{} holding all groundtruth images needed for the evaluation".format(gtDirName))
-    print("{} holding all input images needed for the evaluation".format(inputDirName))
-    print("{} (empty) for temporal storage of lane detected images of one parameter file".format(tmpDirName))
-    sys.exit()
-
-#makes sure we don't overwrite old parameter files
-if os.listdir(paramDirName):
-    print("Error, your directory {} with the parameter files is not empty.".format(paramDirName))
-    print("Do you want to delete everything in this directory? [y/n]")
-    answer = input("> ")
-    if answer == "y":
-        #delete parameter files
-        for the_file in os.listdir(paramDirName):
-            file_path = os.path.join(paramDirName, the_file)
-            try:
-                if os.path.isfile(file_path):
-                    os.unlink(file_path)
-            except Exception as e:
-                print(e)
-        print("Deleted all paramtere files in the directory.")
-    else:
-        print("Not deleting parameter files in the directory. Aborting now...")
-        sys.exit()
-
-
 num_part_range = [2, 3, 4, 5]
 num_lines_range = [2, 3, 4, 5]
 w_num_windows_range = [3, 5, 7, 9, 11]
 w_width_range = [20, 40, 60, 80]
-b_view_range = [0,1]
-filter1_range = [0,1]
-filter2_range = [0,1]
-filter3_range = [0,1]
-filter4_range = [0,1]
+b_view_range = [0, 1]
+filter1_range = [0, 1]
+filter2_range = [0, 1]
+filter3_range = [0, 1]
+filter4_range = [0, 1]
 order_range = [2, 3]
 
 suffix = 0
+
+
 
 def write_params(file, a=-1, np=-1, nl=-1, wnw=-1, ww=-1, bv=-1, f1=-1, f2=-1, f3=-1, f4=-1, o=-1):
     file.write("algo {}\n".format(a))
@@ -76,61 +41,109 @@ def write_params(file, a=-1, np=-1, nl=-1, wnw=-1, ww=-1, bv=-1, f1=-1, f2=-1, f
     file.write("filter4 {}\n".format(f4))
     file.write("order {}\n".format(o))
 
-#Algo 1
-ranges = [num_part_range, b_view_range, filter1_range,
-          filter2_range, filter3_range, filter4_range, order_range]
-a = 1
-for np, bv, f1, f2, f3, f4, o in itertools.product(*ranges):
-    if f1 == 0 and f2 == 0 and f3 == 0 and f4 == 0:
-        continue
-    paramFileName = paramFileNameBase + str(suffix)
-    path = paramDir / Path(paramFileName)
-    with path.open(mode="w") as f:
-        write_params(f, a=a, np=np, bv=bv, f1=f1, f2=f2, f3=f3, f4=f4, o=o)
-    suffix += 1
+def createForAlgo1():
+    #Algo 1
+    global suffix
+    ranges = [num_part_range, b_view_range, filter1_range,
+            filter2_range, filter3_range, filter4_range, order_range]
+    a = 1
+    for np, bv, f1, f2, f3, f4, o in itertools.product(*ranges):
+        if f1 == 0 and f2 == 0 and f3 == 0 and f4 == 0:
+            continue
+        paramFileName = paramFileNameBase + str(suffix) + ".par"
+        path = paramDir / Path(paramFileName)
+        with path.open(mode="w") as f:
+            write_params(f, a=a, np=np, bv=bv, f1=f1, f2=f2, f3=f3, f4=f4, o=o)
+        suffix += 1
+
+def createForAlgo2():
+    #Algo 2
+    global suffix
+    ranges = [num_part_range, num_lines_range, b_view_range, 
+            filter1_range, filter2_range, filter3_range, 
+            filter4_range, order_range]
+    a = 2
+    for np, nl, bv, f1, f2, f3, f4, o in itertools.product(*ranges):
+        if f1 == 0 and f2 == 0 and f3 == 0 and f4 == 0:
+            continue
+        paramFileName = paramFileNameBase + str(suffix) + ".par"
+        path = paramDir / Path(paramFileName)
+        with path.open(mode="w") as f:
+            write_params(f, a=a, np=np, nl=nl, bv=bv, f1=f1, f2=f2, f3=f3, f4=f4, o=o)
+        suffix += 1
 
 
-#Algo 2
-ranges = [num_part_range, num_lines_range, b_view_range, 
-          filter1_range, filter2_range, filter3_range, 
-          filter4_range, order_range]
-a = 2
-for np, nl, bv, f1, f2, f3, f4, o in itertools.product(*ranges):
-    if f1 == 0 and f2 == 0 and f3 == 0 and f4 == 0:
-        continue
-    paramFileName = paramFileNameBase + str(suffix)
-    path = paramDir / Path(paramFileName)
-    with path.open(mode="w") as f:
-        write_params(f, a=a, np=np, nl=nl, bv=bv, f1=f1, f2=f2, f3=f3, f4=f4, o=o)
-    suffix += 1
+def createForAlgo3():
+    #Algo 3
+    global suffix
+    ranges = [w_num_windows_range, w_width_range, b_view_range, 
+            filter1_range, filter2_range, filter3_range, 
+            filter4_range, order_range]
+    a = 3
+    for wnw, ww, bv, f1, f2, f3, f4, o in itertools.product(*ranges):
+        if f1 == 0 and f2 == 0 and f3 == 0 and f4 == 0:
+            continue
+        paramFileName = paramFileNameBase + str(suffix) + ".par"
+        path = paramDir / Path(paramFileName)
+        with path.open(mode="w") as f:
+            write_params(f, a=a, wnw=wnw, ww=ww, bv=bv, f1=f1, f2=f2, f3=f3, f4=f4, o=o)
+        suffix += 1
+
+def createForAlgo4():
+    #Algo 4
+    global suffix
+    ranges = [w_width_range, b_view_range, filter1_range,
+            filter2_range, filter3_range, filter4_range]
+    a = 4
+    for ww, bv, f1, f2, f3, f4 in itertools.product(*ranges):
+        if f1 == 0 and f2 == 0 and f3 == 0 and f4 == 0:
+            continue
+        paramFileName = paramFileNameBase + str(suffix) + ".par"
+        path = paramDir / Path(paramFileName)
+        with path.open(mode="w") as f:
+            write_params(f, a=a, ww=ww, bv=bv, f1=f1, f2=f2, f3=f3, f4=f4)
+        suffix += 1
 
 
-#Algo 3
-ranges = [w_num_windows_range, w_width_range, b_view_range, 
-          filter1_range, filter2_range, filter3_range, 
-          filter4_range, order_range]
-a = 3
-for wnw, ww, bv, f1, f2, f3, f4, o in itertools.product(*ranges):
-    if f1 == 0 and f2 == 0 and f3 == 0 and f4 == 0:
-        continue
-    paramFileName = paramFileNameBase + str(suffix)
-    path = paramDir / Path(paramFileName)
-    with path.open(mode="w") as f:
-        write_params(f, a=a, wnw=wnw, ww=ww, bv=bv, f1=f1, f2=f2, f3=f3, f4=f4, o=o)
-    suffix += 1
+def createParameterFiles():
+
+    if not (resultsDir.is_dir() and gtDir.is_dir() and inputDir.is_dir() and tmpDir.is_dir() and paramDir.is_dir()):
+        print("Error, your directory strcture is wrong! Aborting this script...")
+        print("You need the following structure:")
+        print("{} (empty) for the evaluation measurements files and parameter files".format(
+            resultsDirName))
+        print("{} (empty) for the parameter files generated with this script".format(
+            paramDirName))
+        print("{} holding all groundtruth images needed for the evaluation".format(gtDirName))
+        print("{} holding all input images needed for the evaluation".format(inputDirName))
+        print("{} (empty) for temporal storage of lane detected images of one parameter file".format(tmpDirName))
+        sys.exit()
+
+    #makes sure we don't overwrite old parameter files
+    if os.listdir(paramDirName):
+        print("Error, your directory {} with the parameter files is not empty.".format(
+            paramDirName))
+        print("Do you want to delete everything in this directory? [y/n]")
+        answer = input("> ")
+        if answer == "y":
+            #delete parameter files
+            for the_file in os.listdir(paramDirName):
+                file_path = os.path.join(paramDirName, the_file)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                except Exception as e:
+                    print(e)
+            print("Deleted all paramtere files in the directory.")
+        else:
+            print("Not deleting parameter files in the directory. Aborting now...")
+            sys.exit()
+
+    createForAlgo1()
+    createForAlgo2()
+    createForAlgo3()
+    createForAlgo4()
 
 
-#Algo 4
-ranges = [w_width_range, b_view_range, filter1_range,
-          filter2_range, filter3_range, filter4_range]
-a = 4
-for ww, bv, f1, f2, f3, f4 in itertools.product(*ranges):
-    if f1 == 0 and f2 == 0 and f3 == 0 and f4 == 0:
-        continue
-    paramFileName = paramFileNameBase + str(suffix)
-    path = paramDir / Path(paramFileName)
-    with path.open(mode="w") as f:
-        write_params(f, a=a, ww=ww, bv=bv, f1=f1, f2=f2, f3=f3, f4=f4)
-    suffix += 1
-
-
+if __name__ == '__main__':
+    createParameterFiles()
--- eval/evaluate.py
+++ eval/evaluate.py
@@ -1,34 +1,51 @@
 import os
 import shlex
 from subprocess import Popen, PIPE
+from directoryStructure import *
+import createParamFiles as cpf
 
-binary = "mapra"
-build_path = "../build"
-image_path = "eval_images"
-gts = "groundtruth"
-inputs = "input"
-results = "results"
+#different exit codes of binary
+mapraSuccess = 0
+mapraWarning = -1
+mapraError = 1
 
-def get_pngs(path):
+
+
+def getPngs(path):
     return pngs = [f for f in os.listdir(path) if f.endswith(".png")]
 
+def getParameterFiles(path):
+    return parameterFiles = [f for f in os.listdir(path) if f.endswith(".par")]
 
-def call_binary(image):
-    #../build/mapra eval_images/input *.png eval_images/results 
-    args = shlex.split(os.path.join(build_path, binary) + " " + os.path.joing(image_path, input) + " " + image + " " + os.path.join(image_path, results))
+def callBinary(image):
+    #../build/mapra eval_images/input *.png eval_images/tmp 
+    args = shlex.split(os.path.join(buildPath, binary) + " " + inputDirName + " " + image + " " + tmpDirName)
     proc = Popen(args, stdout=PIPE, stderr=PIPE)
     out, err = proc.communicate
     exitcode = proc.returncode
-    if (exitcode == -1):
+    if (exitcode == mapraWarning):
         print("algorithm could not detect a lane in file %s" %image)
-    if (exitcode == 0):
+    if (exitcode == mapraSuccess):
         print("algorithm detected a lane in file %s" %image)
-    if (exitcode == 1):
+    if (exitcode == mapraError):
         print("algorithm crashed in file %s" %image)
     return exitcode
     
 
+def createParameterFiles():
+    cpf.
+
+'''create file with exit codes form call_binary for each parameter file, save to resultDirName'''
+
 if __name__ == '__main__':
-    input_pngs = get_pngs(os.path.join(image_path, inputs))
-    for png in input_pngs:
-        call_binary(png)
+
+
+    inputPngs = getPngs(inputDirName)
+    #call for all images the binary for one specific parameter file
+    for png in inputPngs:
+        callBinary(png)
+    #=> output images of binary now exist in directory tmpDirName
+    #call evaluateRoad.py on each of output images in tmpDirName
+    #=> one addtional measurement file in resultDirName
+    #delete all images in tmpDirName
+    #advance to next parameter file 
