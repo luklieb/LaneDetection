9437d2d4bc1f12b44fcfeb210331622a24a5d4dd
Lukas L <Lukas@example.com>
2018-04-03 20:26:41 +0200

    added comments

--- eval/print.py
+++ eval/print.py
@@ -30,7 +30,7 @@ def getNumber(name):
         sys.exit()
     return suffix
 
-
+# Increments counter per algorithm, if number is within the range of one algorithm
 def count(number, algosFinished, ranges):
     if(ranges[0].count(number) > 0):
         algosFinished[0] += 1
@@ -47,13 +47,18 @@ def count(number, algosFinished, ranges):
 # the ranges of the 5 algorithms
 def getNumFiles(ranges):
     finished = [0, 0, 0, 0, 0]
+    # Time
+    # Get list of files starting with "time" in directory resultsDirName
     timeFiles = getFiles("time", resultsDirName)
+    # Iterate over individual file names
     for f in timeFiles:
+        # Get the number at end of file name ("timexxx")
         number = getNumber(f)
         count(int(number), finished, ranges)
     print("time: ", finished)
 
     finished = [0, 0, 0, 0, 0]
+    # Exit - analogously
     exitFiles = getFiles("exit", resultsDirName)
     for f in exitFiles:
         number = getNumber(f)
@@ -61,14 +66,17 @@ def getNumFiles(ranges):
     print("exit: ", finished)
 
     finished = [0, 0, 0, 0, 0]
+    # Data - analogously
     resultFiles = getFiles("data", resultsDirName)
     for f in resultFiles:
         number = getNumber(f)
         count(int(number), finished, ranges)
     print("data: ", finished)
 
-
-# Returns list of 5 tuples ( [(0,avgTime)], [(paramNumber, smallestTimes)*num], [(paramNumber, largestTimes)*num])
+# For each of the 5 algorithms (and their ranges) return the average, fastest, slowest time
+# ranges: list of range-objects, one for each algorithm
+# num: number of fastest and slowest configurations (parameter files) to consider
+# Returns list of 5 tuples [5* ([(0,avgTime)], [(paramNumber, fastestTimes)*num], [(paramNumber, slowestTimes)*num]) ]
 def getTimePerAlgo(ranges, num=1):
     assert num > 0
     prefix = "time"
@@ -94,23 +102,30 @@ def getTimePerAlgo(ranges, num=1):
                 probably because the algorithm created only warnings".format(str(curr)))
         times = sorted(times, key=itemgetter(1))
         avgT = sum(v for _, v in times)/len(times)
+        # only save num time measurements
         minT = times[:1+num-1]
         maxT = times[-1-num+1:]
         allTimes.append(( [(0,avgT)], sorted(minT, key=itemgetter(0)), sorted(maxT, key=itemgetter(0))))
     return allTimes
 
 
-# Success/Warning/Error return code ratios
+# Compute Success/Warning/Error return code ratios for each of the 5 algorithms
+# ranges: list of range-objects, one for each algorithm
 # Retruns list with 5 tuples (success ratio in %, warning ratio in %, error ration in %)
-def getRatioWarningsPerAlgo(ranges):
+def getRatioExitCodesPerAlgo(ranges):
     prefix = "exit"
+    # For the 5 tuples
     codeRatios = []
+    # Iterate over the 5 ranges
     for algoRange in ranges:
         warnings = 0
         errors = 0
         successes = 0
+        # Iterate over the range of a specific algorithm
         for curr in algoRange:
             with open(os.path.join(resultsDirName, prefix + str(curr))) as f:
+                # Read all lines of file in
+                # Increment counter for respective exit code
                 for line in f:
                     code = int(line)
                     if code == mapraWarning:
@@ -123,46 +138,65 @@ def getRatioWarningsPerAlgo(ranges):
                         print("wrong code in getRatioWarningsPerAlgo - aborting...")
                         sys.exit()
         codeSum = warnings+errors+successes
+        # Compute ratios in percent and store the tuple for one specific algorithm
         codeRatios.append((successes/codeSum*100., warnings/codeSum*100., errors/codeSum*100.))
     return codeRatios
 
 
-# Computes the averages for each of the 7 figures for the 5 algorithms
-# Returns a nested list with 5 lists of figures (mean) [MaxF, AvgPrec, PRE, REC, TPR, FPR, FNR]
+# Computes only from the num best results the averages for each of the 7 figures for the 5 algorithms 
+# ranges: list of range-objects, one for each algorithm
+# num: number of best (and worst) configurations (parameter files) to consider 
+# Returns a nested list with 5 lists of averaged figures [5*[MaxF, AvgPrec, PRE, REC, TPR, FPR, FNR]]
 def getBestFiguresPerAlgo(ranges, num):
     figures = []
     bestPar = []
     worstPar = []
+    # Iterate over the 5 ranges
     for algoRange in ranges:
         dictData = {}
+        # Iterate over the range of one specifc algorithm
         for curr in algoRange:
+            # Read in the "dataxxx" file as a Panda DataFrame object
             singleData = pd.read_csv(os.path.join(resultsDirName, "data" + str(curr)), sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6], skiprows=[12])
+            # Add DataFrame to a dictionary with the current parameter-file-number curr
             dictData[curr] = singleData
+        # Concatenate multiple DataFrames to a multiindex DataFrame
         multi = pd.concat(dictData)
-        #print(multi)
+        # Compute first mean of the 11 entries of each DataFrame/dataxxx file
         meanPerPar = multi.groupby(level=[0]).mean()
-        #print(meanPerPar)
-        #print("max: ", meanPerPar['#MaxF'].idxmax())
+        # Sort the Dataframe descending according to its values in column "#MaxF"
         meanPerPar.sort_values('#MaxF', inplace=True, ascending=False)
+        # Store num best/worst rows of mean figures according to the "#MaxF" value
         meanPerParBest = meanPerPar.head(num)
         meanPerParWorst = meanPerPar.tail(num)
+        # Store the index values (parameter-file-number) for best/worst mean figures
+        # Used later to manually look at the best/worst parameter files
         bestPar.append(sorted(meanPerParBest.index.values.tolist()))
         worstPar.append(sorted(meanPerParWorst.index.values.tolist()))
+        # Compute second mean in order to get one single row of mean figures per algorithm
         mean = meanPerParBest.mean()
-        #print(mean)
         figures.append(mean.values.tolist())
     return figures, bestPar, worstPar
 
 # Computes the averages for each of the 7 figures for the 5 algorithms
-# Returns a nested list with 5 lists of figures (mean) [MaxF, AvgPrec, PRE, REC, TPR, FPR, FNR]
+# ranges: list of range-objects, one for each algorithm
+# Returns a nested list with 5 lists of averaged figures [5*[MaxF, AvgPrec, PRE, REC, TPR, FPR, FNR]]
 def getAverageFiguresPerAlgo(ranges):
     figures = []
+    # Iterate over the 5 ranges
     for algoRange in ranges:
         dictData = {}
+        # Iterate over the range of one specific algorithm
         for curr in algoRange:
+            # Read in the "dataxxx" file as a Panda DataFrame object
             singleData = pd.read_csv(os.path.join(resultsDirName, "data" + str(curr)), sep=" ", usecols=[0, 1,2,3,4,5,6], skiprows=[12])
+            # Add DataFrame to a dictionary with the current parameter-file-number curr
             dictData[curr] = singleData
+        # Concatenate multiple DataFrames to a multiindex DataFrame
         multi = pd.concat(dictData)
+        # Compute two means in order to get one data row with averaged figures for the specific algorithm
+        # First mean() computes mean of the 11 entries in each data-file
+        # Second mean() computes mean of all the data-file-averages for one algorithm
         mean = multi.groupby(level=[0]).mean().mean()
         figures.append(mean.values.tolist())
     return figures
@@ -177,6 +211,8 @@ def getAverageFiguresPerAlgoForImage(ranges, image=6):
     for algoRange in ranges:
         dictData = {}
         for curr in algoRange:
+            # Same as fct getAverageFiguresPerAlgo
+            # Except that all rows, except row image and row 0 are skipped
             singleData = pd.read_csv(os.path.join(resultsDirName, "data" + str(curr)), sep=" ", usecols=[0, 1, 2, 3, 4, 5, 6], skiprows=lambda x: False if (x==0) else x!=image )
             dictData[curr] = singleData
         multi = pd.concat(dictData)
@@ -184,8 +220,10 @@ def getAverageFiguresPerAlgoForImage(ranges, image=6):
         figures.append(mean.values.tolist())
     return figures
 
-# Makes a bar plot.
-# Parameter figure should be list of 5 (=algos) lists with each 7 (=figures) values
+# Makes and shows a bar plot for averaged figures from fct calls like:
+# getAverageFiguresPerAlgoForImage(), getAverageFiguresPerAlgo(), getBestFiguresPerAlg()
+# figure: should be list of 5 (=number algorithms) lists with each 7 (=number of figures) values
+# title: title to add to the plot
 def plotAverageFigures(figure, title):
     x = np.arange(7)  # 7figures
     for ind, f in enumerate(figure):
@@ -197,28 +235,40 @@ def plotAverageFigures(figure, title):
     plt.suptitle(title)
     plt.show()
 
+# Makes and shows a bar plot for the ratios from fct calls like:
+# getRatioExitCodesPerAlgo()
+# ratios: list with 5 (=number of algorithsm) tuples (success ratio, warning ratio, error ratio)
 def plotRatios(ratios):
     x = np.arange(3)  # 3 different warnings
+    # Iterate over the 5 algorithms
     for ind, r in enumerate(ratios):
+        # Plot bar give each bar an offset of 0.1
         plt.bar(x+(-2+ind)*0.1, r, width=0.1)
     plt.xticks(x, ("Success", "Warning", "Error"))
-    plt.legend(("Part. Hough", "ALM", "Sliding Windows",
-                "Fixed Windows", "Random"))
+    plt.legend(("Part. Hough", "ALM", "Sliding Windows", "Fixed Windows", "Random"))
     plt.ylabel("%")
     plt.suptitle("Exit Codes - 'Robustness'")
     plt.show()
 
-# Plots the time measuremnts
+# Plots the time measuremnts from fct calls like:
+# getTimePerAlgo()
+# times: list of 5 tuples [5*([(0,avgTime)], [(paramNumber, fastestTimes)*num], [(paramNumber, slowestTimes)*num])]
 def plotTimes(times):
     x = np.arange(3)  # 3 different time types (fastest, mean, slowest)
+    # Iterate over the 5 algorithms
     for ind, t in enumerate(times):
+        # From each of the 5 tuples generate a new list [fastest, mean, slowest]
+        # Plot a bar plot
         plt.bar(x+(-2+ind)*0.1, [t[1][0][1], t[0][0][1], t[2][0][1]], width=0.1)
     plt.legend(("Part. Hough", "ALM", "Sliding Windows","Fixed Windows", "Random"))
     plt.xticks(x, ("Fastest", "Mean", "Slowest"))
-    #for i, ind in enumerate([1,0,2]):
-    #    plt.bar(x+(-1+i)*0.2, [times[x][ind][0][1] for x in range(5)], width=0.2)
-    #plt.legend(("Fastest", "Mean", "Slowest"))
-    #plt.xticks(x, ("Part. Hough", "ALM", "Sliding Windows","Fixed Windows", "Random"))
+    '''
+    # "Inverted": also change x = np.arange(5)
+    for i, ind in enumerate([1,0,2]):
+        plt.bar(x+(-1+i)*0.2, [times[x][ind][0][1] for x in range(5)], width=0.2)
+    plt.legend(("Fastest", "Mean", "Slowest"))
+    plt.xticks(x, ("Part. Hough", "ALM", "Sliding Windows","Fixed Windows", "Random"))
+    '''
     plt.ylabel("sec")
     plt.grid(True, 'both', axis="y", linewidth=1, linestyle=':', alpha=0.6)
     #plt.gca().set_ylim(0.3)
@@ -269,7 +319,7 @@ def main():
 
     #figure = getAverageFiguresPerAlgoForImage(ranges, 6)
     #plotAverageFigures(figure, "Average Figures For Image 6")
-    fig, best, worst = getBestFiguresPerAlgo(ranges, 20)
+    fig, best, worst = getBestFiguresPerAlgo(ranges, 50)
     print(fig)
     print(best)
     print(worst)
